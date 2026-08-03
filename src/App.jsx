cb655a4c3de59c11c049cbb23a9ab61e3e9457bb
Jefferson Santos <jeffrrwpg678@gmail.com>
2026-08-03 16:39:25 -0300

    Add Landing Page Styles with Responsive Design and Theming Tokens

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { Routes, Route } from "react-router-dom";
 import CurriculumEditor from "./components/CurriculumEditor";
 import ThemeProvider from "./provider/ThemeProvider";
 import FontProvider from "./provider/FontProvider";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <FontProvider>
-        <ThemeProvider>
-          <CurriculumEditor />
-        </ThemeProvider>
-      </FontProvider>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route
+        path="/editor"
+        element={
+          <div className="App">
+            <FontProvider>
+              <ThemeProvider>
+                <CurriculumEditor />
+              </ThemeProvider>
+            </FontProvider>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import '../src/assets/css/styles.css';
-import 'notyf/notyf.min.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "../src/assets/css/styles.css";
+import "notyf/notyf.min.css";
 
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service_worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado com sucesso:', registration);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service_worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado com sucesso:", registration);
       })
-      .catch(error => {
-        console.error('Falha ao registrar o Service Worker:', error);
+      .catch((error) => {
+        console.error("Falha ao registrar o Service Worker:", error);
       });
   });
-
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
